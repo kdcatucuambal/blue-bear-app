2310dbfa6955d4e60095539ffd21e9491ae63749
Kevin Catucuamba <kdcatucuambal@utn.edu.ec>
2024-04-04 08:17:11 -0500

    KEC  - Fix regex

--- src/app/util/app.util.ts
+++ src/app/util/app.util.ts
@@ -1,10 +1,9 @@
 export const validateEmail = (email: string): boolean=>{
-  //const regexEmail = /^([\da-z_.-]+)@([\da-z.-]+)\.([a-z.]{2,6})$/;
   const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
   return !!regexEmail.exec(email);
 }
 
 export const validatePassword = (password: string): boolean=>{
-  const regexPwd  = /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/;
+  const regexPwd  = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
   return !!regexPwd.exec(password);
 }
